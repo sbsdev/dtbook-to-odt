f9c4cf857a0976d25b18585015a2b3e7f9fded84
Bert Frees <bert.frees@sbs.ch>
2014-01-14 11:28:50 +0100

    Update .dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,11 @@
-;;; Directory Local Variables
-;;; See Info node `(emacs) Directory Variables' for more information.
-
-((dired-mode
-  (compile-command . "mvn clean package -P deb")))
+((nil
+  . ((compile-command . (format "cd %s && mvn clean package"
+                                (locate-dominating-file buffer-file-name "pom.xml")))))
+ ("src/main/resources/xml/dtbook-to-odt.xpl"
+  . ((nil . ((indent-tabs-mode . nil)
+             (tab-width . 4)
+             (nxml-child-indent . 4)))))
+ ("src/main/resources/xml/content-sbs.xsl"
+  . ((nil . ((indent-tabs-mode . t)
+             (tab-width . 4)
+             (nxml-child-indent . 4))))))
